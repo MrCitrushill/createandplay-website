92fda7e83152c657bb686e8d1f1361cf1164f512
MrCitrushill <55997589+MrCitrushill@users.noreply.github.com>
2022-06-10 15:42:12 +0200

    creating index

---
+++ .github/index.php
@@ -0,0 +1,391 @@
+<html lang="de">
+  <head>
+
+    <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-M4S8JF5');</script>
+    <!-- End Google Tag Manager -->
+
+  <title>Create 'n' Play - Dein Twitch-Team für alle Interessen!</title>
+
+  <!-- favicons -->
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=XBbneYm7Wz">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=XBbneYm7Wz">
+  <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=XBbneYm7Wz">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=XBbneYm7Wz">
+  <link rel="manifest" href="/site.webmanifest?v=XBbneYm7Wz">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg?v=XBbneYm7Wz" color="#603cba">
+  <link rel="shortcut icon" href="/favicon.ico?v=XBbneYm7Wz">
+  <meta name="msapplication-TileColor" content="#603cba">
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=XBbneYm7Wz">
+  <meta name="theme-color" content="#00acc1">
+
+  <!-- Twitter Card & OG Tags -->
+
+  <meta property="og:title" content="Langeweile? Nicht bei Create 'n' Play!" />
+  <meta property="og:description" content="Schau bei einem unserer Twitch-Streamer vorbei und genieße die Show. Egal ob Creative oder Gaming, bei uns findest du alles." />
+  <meta property="og:image" content="https://createandplay.de/images/ogimage.png">
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="de_DE" />
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@TEAM_CNP">
+  <meta name="twitter:title" content="Langeweile? Nicht bei Create 'n' Play!">
+  <meta name="twitter:description" content="Schau bei einem unserer Twitch-Streamer vorbei und genieße die Show. Egal ob Creative oder Gaming, bei uns findest du alles.">
+  <meta name="twitter:image" content="https://createandplay.de/images/ogimage.png">
+
+  <!-- Required meta tags -->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, height=height, initial-scale=1.0 user-scalable=no">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Bungee|Open+Sans&display=swap" rel="stylesheet">
+
+  <!-- bootstrapcdn -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+  <!-- styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/waves.css">
+
+
+
+  <!-- scripts -->
+  <script src="https://kit.fontawesome.com/fc3e91053e.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"></script>
+  <script> var scroll = new SmoothScroll('a[href*="#"]'); </script>
+
+
+
+  <!-- php -->
+  <?php
+  include 'include/api.php';
+  include 'include/ndb.php';
+
+// Create connection
+
+  //include 'include/db.php';
+/*
+  function isOnline($id) {
+    if (stream($id)['data'][0]['type'] == 'live') {
+      echo '<p class="online"><span class="live">LIVE!</span></p>';
+    } else {
+      echo '<p class="offline">Offline</p>';
+    }
+  }
+
+  function commissions($id) {
+    if (getDatabase($id)['u_commissions'] == 2) {
+      echo '<p class="commissions"><span class="open">Commissions: OPEN</span></p>';
+    } elseif (getDatabase($id)['u_commissions'] == 1) {
+      echo '<p class="commissions"><span class="closed">Commissions: CLOSED</span></p>';
+    } else {
+    }
+  }
+*/
+
+
+
+
+  //theartoft 89580373
+  //huschee 79970021
+  //justedris 16345254
+  //directorkoala 63963512
+  //hatschyuh 147124884
+  //f0eh 95082092
+  //d00m2k7 52714716
+  //traumfalke 52742709
+  //frauschnabeltier 124722278
+  //angryhidden 92551111
+  //lizzplays 191126513
+  //trashangel 76301822
+  //i7 95703532
+  ?>
+
+
+
+  </head>
+
+  <body>
+
+    <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4S8JF5"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+  <!--Content starts-->
+<nav class="menu navbar fixed-top navbar-light">
+  <div id="navbarNav" class="row col-sm-8">
+    <ul class="navbar-nav col-sm-12 u-center-text">
+      <li class="nav-item col-sm-2">
+        <a class="nav-link wasistcnp" onclick="window.scrollTo({ top: 433, left: 0, behavior: 'smooth' });">Was ist C'n'P?</a>
+      </li>
+      <li class="nav-item col-sm-2">
+        <a class="nav-link zahlen" onclick="window.scrollTo({ top: 1018, left: 0, behavior: 'smooth' });">Zahlen</a>
+      </li>
+      <li class="nav-item col-sm-4">
+        <img class="logoheader-top" src="images/head_color.png" alt="C'n'P Logo">
+      </li>
+      <li class="nav-item col-sm-2">
+        <a class="nav-link streamer" onclick="window.scrollTo({ top: 1426, left: 0, behavior: 'smooth' });">Streamer</a>
+      </li>
+      <!--<li class="nav-item col-sm-2">
+        <a class="nav-link contactbox" data-toggle="modal" data-target="#contactModal">Kontakt</a>
+      </li>-->
+    </ul>
+  </div>
+</nav>
+
+<section class="section-plans" id="section-plans">
+  <div id="background">
+<div class="header">
+  <div class="row u-center-text">
+    <img class="logoheader top" src="images/logo.webp" alt="Create 'n' Play Logo">
+  </div>
+</div>
+
+      <div class="u-center-text u-margin-bottom-big">
+        <h2 id="wasistcnp" class="heading-secondary">
+          Was ist Create 'n' Play?
+        </h2>
+    <div class="spacer" data-type="group"><span class="fa before"></span><i class="fa fa-circle-thin"></i><span class="fa after"></span></div>
+      </div>
+
+    <div class="row who content">
+    <div class="container" style="padding-bottom: 1rem;">
+          <p>Willkommen bei <b>Create 'n' Play</b>!</p>
+          <p>Wir sind ein Team aus <b>Gamern</b> und <b>Kreativen</b>; aus den Bereichen <b>Illustration</b>, <b>Kunst</b> und <b>Handwerk</b>. Unseren Zuschauern bieten wir eine breite Palette an Unterhaltung und Abwechslung. Hier ist garantiert für jeden etwas dabei!</p>
+    </div>
+            <div class="container">
+              <p>Du möchtest mehr über uns erfahren? Dann schau doch mal auf unseren Social Media Kanälen, im Discord oder auf Twitch vorbei:</p>
+              <div class="row" style="padding-top: 1rem; display: inline-flex;">
+                <a id="Create-n-Play-Twitter" class="socialbuttons" href="https://twitter.com/TEAM_CNP" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a id="Create-n-Play-Facebook" class="socialbuttons" href="https://www.facebook.com/teamcnp" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a id="Create-n-Play-Instagram" class="socialbuttons" href="https://www.instagram.com/teamcnp/" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a id="Create-n-Play-Discord" class="socialbuttons" href="https://discord.gg/MkUmS5F" target="_blank"><i class="fab fa-discord"></i></a>
+                <a id="Create-n-Play-Twitch" class="socialbuttons" href="https://www.twitch.tv/team/create_and_play" target="_blank"><i class="fab fa-twitch"></i></a>
+              </div>
+            </div>
+
+
+    </div>
+
+      <div class="u-center-text u-margin-bottom-big">
+        <h2 id="zahlen" class="heading-secondary">
+          Unsere Zahlen
+        </h2>
+    <div class="spacer" data-type="group"><span class="fa before"></span><i class="fa fa-circle-thin"></i><span class="fa after"></span></div>
+      </div>
+
+      <div class="row">
+        <div class="counter col_fourth u-center-text">
+          <img src="images/users-solid.png" style="height:30px;">
+          <h2 class="timer count-title count-number" data-to="1" data-speed="1500"></h2>
+          <p class="count-text ">Team</p>
+        </div>
+        <div class="counter col_fourth u-center-text">
+          <img src="images/user-solid.png" style="height:30px;">
+          <h2 class="timer count-title count-number" data-to="<?php echo getAllStreamer(); ?>" data-speed="1500"></h2>
+           <p class="count-text ">Mitglieder</p>
+        </div>
+        <div class="counter col_fourth u-center-text">
+          <img src="images/thumbs-up-solid.png" style="height:30px;">
+          <h2 class="timer count-title count-number" data-to="<?php echo getAllFollower(); ?>" data-speed="1500"></h2>
+          <p class="count-text ">Follower</p>
+        </div>
+        <div class="counter col_fourth u-center-text end">
+          <img src="images/eye-solid.png" style="height:30px;">
+          <h2 class="timer count-title count-number" data-to="<?php echo getAllViews(); ?>" data-speed="1500"></h2>
+          <p class="count-text ">Views</p>
+        </div>
+      </div>
+
+
+<!-- ________________________________________________________ -->
+<!-- ______________________ Streamer ________________________ -->
+<!-- ________________________________________________________ -->
+
+<div class="u-center-text u-margin-bottom-big">
+  <h2 id="streamer" class="heading-secondary">
+    Unsere Streamer
+  </h2>
+<div class="spacer" data-type="group"><span class="fa before"></span><i class="fa fa-circle-thin"></i><span class="fa after"></span></div>
+</div>
+<?php
+$i = 0;
+$pdo = getConnection();
+$sql = "SELECT * FROM twitchusers";
+
+foreach ($pdo->query($sql) as $row) {
+
+if($row['streamer'] == 1) {
+$stats = getStatistiken($row['t_id']);
+if ($i === 0): ?>
+  <div class="row">
+<?php endif; ?>
+
+<div class="col col-xs-12 col-sm-6 col-xl-6">
+  <div class="card">
+    <div class="card__side card__side--front-1">
+      <div class="card__title card__title--1">
+        <img src="<?php echo $row['t_picture']; ?>" class="card-img-top" alt="...">
+        <h4 class="card__heading"><?php echo $row['t_username'] ?> <br/><?php /* isOnline($row['t_id']); */ ?></h4>
+        <div class="gamingcreative"></div>
+        <img class="<?php echo getThemeClass($row['t_id']); ?>" src="<?php echo getThemePicture($row['t_id']) ?>">
+      </div>
+
+      <div class="card__details">
+        <p class="desc"><?php echo $row['t_description']; ?></p>
+      </div>
+    </div>
+    <div class="card__side card__side--back card__side--back-1">
+      <div class="card__cta">
+        <div class="card__info">
+          <div class="row" style="height: 20vh;">
+            <div class="col-sm-4 stats">
+              <h5>Follower:</h5>
+              <p>> <?php echo $stats['s_follower']; ?></p>
+              <h5>Views:</h5>
+              <p>> <?php echo $stats['s_views']; ?></p>
+            </div>
+            <div class="contact">
+                <h5>Social Media</h5>
+                <?php echo getSocial($row['t_id']) ?>
+                <br/>
+
+
+                <div class="u-center-text u-margin-top-huge" style="margin-top: 1.3rem;">
+                  <h5>Lust auf mehr?</h5>
+                  <a id="<?php echo $row['t_username'] . ' | Twitch' ?>" href="https://twitch.tv/<?php echo $row['t_username'] ?>" class="btn btncnp" target="_blank">Zum Stream</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php if ($i === 1): ?>
+  </div>
+  <? $i = 0; ?>
+<?php else: ?>
+  <?php $i = 1; ?>
+<?php endif; ?>
+<?php } } ?>
+
+<?php if ($i === 1): ?>
+</div>
+<?php endif; ?>
+
+
+      <!--Waves Container-->
+      <div>
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <defs>
+      <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g class="parallax">
+      <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+      <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+      <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+      <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+      </g>
+      </svg>
+
+      <div class="footer">
+        <p>Copyright &copy; <?php echo date("Y"); ?> by <a href="https://citrushill.de" target="_blank">MrCitrushill<!--<img src="images/lemondesigns_01.png" alt="MrCitrushill" style="height: 1.5rem; margin-bottom: 0.1rem;">--></a> | <a href="impressum.php">Impressum</a></p>
+      </div>
+
+      </div>
+      <!--Waves end-->
+
+    </div>
+    </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="contactModalLabel">Kontaktformular</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+            <p>Du möchtest Kontakt zu uns aufnehmen? Dann los! Wir freuen uns über deine Nachricht!</p>
+            <br />
+
+            <iframe src="contact/kontakt.php" style="border: none; width:100%; height:700px; margin: 0 0 0 1%;"></iframe>
+
+          </div>
+        </div>
+      </div>
+</div>
+
+<div class="cookie-banner" style="display: none">
+  <p>Mit Nutzung der Webseite stimmst du unserer <a id="Impressum" href="https://createandplay.de/impressum.php" target="_blank">Datenschutzbestimmung</a> zu.</p>
+  <button class="closecookie btncnp">Hinweis schließen</button>
+</div>
+
+
+      <!-- bootstrapcdn popper and jquery -->
+      <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+      <!-- scrollmagiccdn -->
+      <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+
+    <!-- script -->
+    <!-- <script src="js/getImages.js"></script> -->
+    <script src="js/counter.js"></script>
+    <script src="js/scrollspy.js"></script>
+    <script src="js/main.js"></script>
+
+
+
+    <script>var online = document.getElementsByClassName('online'); Array.from(online).forEach((el) => {el.parentNode.parentNode.parentNode.classList.add("isOnline");});</script>
+
+    <!-- scroll -->
+    <script>
+      $(document).ready(function() {
+        $(window).scroll(function() {
+          var height = $('.navbar').height();
+          var scrollTop = $(window).scrollTop();
+
+          if (scrollTop >= 80) {
+            $('.navbar').addClass('top');
+            $('.logoheader-top').addClass('showHeader');
+          } else {
+            $('.navbar').removeClass('top');
+            $('.logoheader-top').removeClass('showHeader');
+          }
+        });
+      });
+
+      </script>
+<script>
+      if(localStorage.getItem('cookieSeen') != 'shown'){
+    $(".cookie-banner").delay(2000).fadeIn();
+    localStorage.setItem('cookieSeen','shown')
+    }
+
+    $('.closecookie').click(function(e) {
+      $('.cookie-banner').fadeOut();
+    });
+
+    </script>
+
+
+
+  </body>
+</html>
